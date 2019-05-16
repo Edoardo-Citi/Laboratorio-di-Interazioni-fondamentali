da98e79cc1242728f8169c5c62af1034e0d34633
edoardo citi <edoardociti@gmail.com>
2019-05-16 17:50:11 +0200

    Versione funzionante e stampante del Montecarlo

--- Tiny_x_MC.c
+++ Tiny_x_MC.c
@@ -54,7 +54,7 @@ Double_t Dcy = 0.02;
 
 void Tiny_x_MC(){
 	//Counter e numero eventi da generare
-	unsigned i=0, N = 1e8;
+	unsigned i=0, N = 1e7;
 	//Counter vari
 	unsigned Missed =0;
 	
@@ -64,16 +64,18 @@ void Tiny_x_MC(){
 	//Variabili dell'evento
 	Double_t Xu=0, Yu=0, Xd=0, Yd=0, C_Theta=0, Phi=0;
 	//Variabile di un evento andato a segno
-	Double_t E=0, beta_mu=0, Ts=0, Td=0, Vs=0, Vd=0;
+	Double_t E=0, beta_mu=0, Ts=0, Td=0, Vs=0, Vd=0, temp1=0,temp2=0, ratio=0.1,Tcen=0;
 	
-	TH1F* histo = new TH1F ("histo", "histo", 10000, 25 1e-9, 35* 1e-9);
-	
+	TH1F* histo = new TH1F ("histo", "histo", 1000, -25e-9, 25* 1e-9);
+	TH1F* histo2 = new TH1F ("histo2", "histo2", 1000, -25e-9, 25* 1e-9);
+	TH1F* histo3 = new TH1F ("histo2", "histo2", 1000, -25e-9, 25* 1e-9);
+	Tcen = (Ux - 2 * Dcx)/beta_s ;
 	//Ciclo di generazione
 	for (i=0; i<N; i++){
 		
 		//Estraggo posizione e direzione iniziale
-		Xu = (Rx -> Rndm()) * Dx + Dcx;
-		Yu = (Rx -> Rndm()) * Dy + Dcy;
+		Xu = ((Rx -> Rndm())-0.5) * Dx + Dcx;
+		Yu = ((Rx -> Rndm())-0.5) * Dy + Dcy;
 		C_Theta =TMath::Power(Rx -> Rndm(),1.0/3.0);
 		Phi = (Rx -> Rndm()) * 2 * (TMath::Pi());
 		
@@ -83,11 +85,21 @@ void Tiny_x_MC(){
 		if(0 <= Xd && Xd<= Ux && 0 <= Yd && Yd <= Uy){
 			
 			//Genero i tempi in lettura nella barra
-			Ts = (Ux - 2 * Xd)/beta_s +30 * 1e-9;
+			Ts = (Ux - 2 * Xd)/beta_s ;
+			if(Rx -> Rndm()>ratio){ temp1=Rx->Gaus(0,1.7e-9);}
+			else{temp1=Rx->Gaus(0,6*1e-9);}
+			if(Rx -> Rndm()>ratio){ temp2=Rx->Gaus(0,1.7e-9);}
+			else{temp2=Rx->Gaus(0,6*1e-9);}
+			histo2 -> Fill(Tcen+temp1+temp2);
 			histo -> Fill (Ts);
+			histo3 -> Fill(temp1+temp2+Ts);
 
 		}
 		else {Missed ++;}
 	}
-histo -> Draw();
+//histo -> Draw();
+histo2 -> Draw();
+histo2-> SetLineColor(2);
+histo3 -> Draw("same");
+histo3-> SetLineColor(3);
 }
